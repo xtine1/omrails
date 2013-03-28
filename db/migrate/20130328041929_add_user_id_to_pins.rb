5ddc6c0f9894021f94240f5785f8d737abb0c2d8
Christine Lee <christinee.lee@gmail.com>
2013-03-28 00:44:11 -0400

    Add Pins Assocation

--- db/migrate/20130328041929_add_user_id_to_pins.rb
+++ db/migrate/20130328041929_add_user_id_to_pins.rb
@@ -1,4 +1,4 @@
-class AddNameToUsers < ActiveRecord::Migration
+class AddUserIdToPins < ActiveRecord::Migration
   def change
     add_column :pins, :user_id, :integer
     add_index :pins, :user_id
